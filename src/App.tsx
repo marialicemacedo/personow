fad5a8650742ceb0defc2ded6b1b28554c4cbc4f
alimcedo <marialicemacedo@alu.ufc.br>
2022-05-19 11:07:43 -0300

    add route to register

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import { Home } from "./components/Home";
+import { Register } from "./components/Register";
 
 function App() {
   return (
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/home" element={<Home/>}/>
+        <Route path="/register" element={<Register/>}/>
       </Routes>
   );
 }
